dbb9f9a2f18bafd41ace4e57b80f5f5780ee9ad1
Andrew Lounsbury <lounsburya@gmail.com>
2022-03-19 14:48:35 -0500

    finished server js GET

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,20 @@ const todoRouter = express.Router();
 const pool = require('../modules/pool.js');
 
 
-todoRouter.get()
+todoRouter.get('/', (req, res) => {
+    console.log('In router GET');
+    let queryText = `
+    SELECT * FROM "todo"
+    ORDER BY "task"
+    `;
+pool.query(queryText).then(todo =>{
+    res.send(todo.rows);
+
+}).catch(error =>{
+    console.log('error getting todo', error);
+    res.sendStatus(500);
+})
+})
 
 
 module.exports = todoRouter;
